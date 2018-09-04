f50518d455cd14dad3db48d62e49079d7842c6d1
lostfind <kpdzzang@gmail.com>
2018-09-04 16:57:13 +0900

    機能追加
    スレッド、コメントの更新/削除 機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show, :update, :destroy]
+
   def new
     @post = Post.new
+    @current_forum = Forum.find_by_forum_id(params[:forum_id])
+    @parent_forum = Forum.find_by_forum_id(@current_forum.parent_forum_id)
   end
 
   def create
@@ -15,14 +19,41 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find_by_post_id(params[:id])
     @current_forum = Forum.find_by_forum_id(@post.forum_id)
     @parent_forum = Forum.find_by_forum_id(@current_forum.parent_forum_id)
     @replies = Reply.reply_list(@post.post_id)
     @reply = Reply.new
+    @reply.post_id = @post.post_id
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to post_path(@post.post_id) }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @post.destroy
+        format.html { redirect_to forum_path(@post.forum_id) }
+      else
+        format.html { render :show }
+      end
+    end
   end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:forum_id, :title, :content, :post_user_id, :close_dttm)
   end
--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,18 +1,45 @@
 class RepliesController < ApplicationController
+  before_action :set_reply, only: [:edit, :update, :destroy]
+
   def create
     @reply = Reply.new(reply_params)
+
     unless @reply.content.to_s.length == 0
       respond_to do |format|
         if @reply.save
-          format.html { redirect_to post_path(id: @reply.post_id), notice: 'Reply Posted!'}
+          format.html { redirect_to post_path(id: @reply.post_id), notice: 'Reply Posted!' }
         else
-          format.html { render :new }
+          format.html { redirect_to post_path(id: @reply.post_id) }
         end
       end
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @reply.update(reply_params)
+        format.html { redirect_to post_path(@reply.post_id) }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      @reply.destroy
+      format.html { redirect_to post_path(@reply.post_id) }
+    end
+  end
+
   private
+  def set_reply
+    @reply = Reply.find(params[:id])
+  end
+
   def reply_params
     params.require(:reply).permit(:post_id, :content, :user_id, :quote_reply_id, :lvl)
   end
